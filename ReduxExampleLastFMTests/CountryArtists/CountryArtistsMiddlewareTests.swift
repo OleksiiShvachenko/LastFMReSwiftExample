a014b21f1174bf5c1542ee6cfadd1b2aafb5ba2e
Oleksii Shvachenko <saol@ciklum.com>
2018-09-17 12:12:53 +0300

    update test.

--- ReduxExampleLastFMTests/CountryArtists/CountryArtistsMiddlewareTests.swift
+++ ReduxExampleLastFMTests/CountryArtists/CountryArtistsMiddlewareTests.swift
@@ -11,20 +11,22 @@ import ReSwift
 @testable import LastFMReduxState
 
 class CountryArtistsMiddlewareTests: BaseMiddlewareTests {
+  
   func testLoadArtistsMiddleware() {
     let stubCountry = Country.ukraine
     let api = API.topArtists(for: stubCountry.rawValue)
-    let error = NetworkError.noInternetConnection("Test")
-    let result = StubResult.error(error)
+    let result = StubResult.succes(filename: "ArtistsResult")
     let artistsStub = Stub(result: result, url: api.url, method: api.method)
     let sessionManager = StubSessionManager([artistsStub])
     sut = loadArtists(service: sessionManager)
     _ = sut(CountryArtistsState.Actions.loadArtists(stubCountry), context)
-    guard let action = dispatchedAction as? CountryArtistsState.Actions,
+    guard
+      let action = dispatchedAction as? CountryArtistsState.Actions,
       case .setArtists(let artists) = action else {
         XCTFail("wrong action was dispathced")
         return
     }
     XCTAssertTrue(artists.count > 0)
   }
+  
 }
